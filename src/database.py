bf8392a878c1d6c2dfb3518cb7dcd5a6cdf94eb9
glmzhn <galymzhan2007@gmail.com>
2024-10-17 23:33:59 +0500

    First

---
+++ src/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import URL, create_engine, text
+from config import settings
+
+engine = create_engine(
+    url=settings.database_url_psycopg,
+    echo=True,
+    # pool_size=5,
+    # max_overflow=10
+)
+
+with engine.connect() as conn:
+    res = conn.execute(text("SELECT VERSION()"))
+    print(f'{res=}')
