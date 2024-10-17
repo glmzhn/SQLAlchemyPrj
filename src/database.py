3f22c4c5e93865710499b80c1f71a44e31bb89ce
glmzhn <galymzhan2007@gmail.com>
2024-10-17 23:47:45 +0500

    Second

--- src/database.py
+++ src/database.py
@@ -1,15 +1,24 @@
+import asyncio
+
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import URL, create_engine, text
 from config import settings
 
-engine = create_engine(
+sync_engine = create_engine(
     url=settings.database_url_psycopg,
     echo=True,
-    # pool_size=5,
-    # max_overflow=10
 )
 
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT VERSION()"))
-    print(f'{res=}')
+async_engine = create_async_engine(
+    url=settings.database_url_asyncpg,
+    echo=True,
+)
+
+
+async def get_123():
+    async with async_engine.connect() as conn:
+        res = await conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
+        print(f'{res.first()=}')
+
+asyncio.run(get_123())
